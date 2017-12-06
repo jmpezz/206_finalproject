af1fc297f5ae7f2afc24a95d2002d8ae10785d31
Julia Pezzullo <jmpezz@umich.edu>
2017-12-06 14:14:39 -0500

    database created

--- finalproj.py
+++ finalproj.py
@@ -3,11 +3,12 @@
 import itertools
 import json
 import sqlite3
+import datetime
 from urllib.request import urlopen
 
 access_token = "25880197.9d22fc1.264658484e9142b190635083bfd474aa"
 
-#time, caption length, likes, filter, hashtags, comments, type, location, user tags
+#time, caption length, likes, hashtags, comments, type, location, user tags
 #
 with urlopen('https://api.instagram.com/v1/users/self/media/recent/?access_token=25880197.9d22fc1.264658484e9142b190635083bfd474aa') as response:
 	str_response = response.read().decode('utf-8')
@@ -17,7 +18,7 @@ with urlopen('https://api.instagram.com/v1/users/self/media/recent/?access_token
 		print (ob)
 
 CACHE_FNAME = "206_FinalProject_cache.json"
-# Put the rest of your caching setup here:
+
 try:
 	cache_file = open(CACHE_FNAME, 'r') #attempting to read data from file
 	cache_data = cache_file.read() #if data there, get into string
@@ -25,5 +26,39 @@ try:
 	CACHE_DICTION = json.loads(cache_data) #load data into dictionary
 except:
 	CACHE_DICTION = {}
+
+
+conn = sqlite3.connect('206_FinalProject.sqlite')
+cur = conn.cursor()
+
+cur.execute('DROP TABLE IF EXISTS InstagramDB')
+cur.execute('CREATE TABLE InstagramDB (date_time DATETIME, caption_length INTEGER, likes INTEGER, tags TEXT, comments INTEGER, type TEXT, location TEXT, tagged_users TEXT)')
+
+for info in obj['data']:
+	date_posted = datetime.datetime.fromtimestamp(int(str(info['created_time'])))
+	caption_len = len(info['caption']['text'])
+	likes = info['likes']['count']
+	hashtags = info['tags']
+	comments = info['comments']['count']
+	type_post = info['type']
+	try:
+		location = info['location']['name']
+	except:
+		location = 'No Named Location'
+	if len(info['users_in_photo']) == 0:
+		tagged_users = "No tagged users"
+
+	else:
+		for user in info['users_in_photo']:
+			tagged_users= []
+			tagged_users.append(user['user']['full_name'])
+
+
+			#tagged_users.append(user['user']['full_name'])
+	print (tagged_users)
+
+
+
+
 
 
